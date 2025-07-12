7261f4e2ab6a1c9b8df1fa122d8d5acae372b242
Parham Abbasi <prmbas@gmail.com>
2025-07-12 15:44:47 +0330

    OK

--- quad.go
+++ quad.go
@@ -8,18 +8,16 @@ func quad(a float64, b float64, c float64, x float64) float64 {
 	return a * math.Pow(x, 2) + b * x + c
 }
 
-func solve(a float64, b float64, c float64) (float64, float64) {
+func solve(a float64, b float64, c float64) (float64, float64, error) {
 	delta := math.Pow(b, 2) - 4 * a * c
 	if delta < 0 {
-		errors.New("can't work with 42")
-		return 0, 0
+		return 0., 0., errors.New("Negative delta")
 	} else {
-		return (-b + math.Sqrt(delta))/2/a, (-b + math.Sqrt(delta))/2/a
+		return (-b + math.Sqrt(delta))/2/a, (-b - math.Sqrt(delta))/2/a, nil
 	}
-	return 0, 0
 }
 
 func main() {
 	// fmt.Println(quad(1., 1., 1., 1.))
-	fmt.Println(solve(1., 0., -4.))
+	fmt.Println(solve(1., 4., +16.))
 }
